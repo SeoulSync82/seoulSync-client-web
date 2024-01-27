d36dfa71727111f409d690fbe298e28d6c95edb7
henry <redbee99@naver.com>
2024-01-27 20:17:08 +0900

    🔧 (chore): 포트 번호 변경

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     target: 'esnext',
@@ -12,5 +11,8 @@ export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
+  },
+  server: {
+    port: 3000,
   },
 });
