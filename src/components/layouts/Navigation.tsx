e94edba5900e361415122ed668fbb8426318bc2e
henry <redbee99@naver.com>
2024-01-27 20:12:47 +0900

    ✨ (feat): 네비게이션 레이아웃 작업 완료

--- src/components/layouts/Navigation.tsx
+++ src/components/layouts/Navigation.tsx
@@ -1,18 +1,58 @@
+import { useState } from 'react';
+import SVGIcon, { IconType } from '../atoms/SVGIcon';
+
+type menuType = {
+  key: IconType;
+  name: string;
+  size: number;
+};
+
 const Navigation = () => {
+  const [activeMenu, setActiveMenu] = useState('home');
+
+  const menuList: Array<menuType> = [
+    {
+      key: 'home',
+      name: '홈',
+      size: 24,
+    },
+    {
+      key: 'myCourse',
+      name: '내 코스',
+      size: 24,
+    },
+  ];
+
+  const onClickMenu = (menu: string) => {
+    setActiveMenu(menu);
+  };
+
+  const makeNavigation = (): JSX.Element[] => {
+    const result: JSX.Element[] = [];
+    menuList.forEach((item) => {
+      result.push(
+        <li
+          className={`group flex grow cursor-pointer flex-col items-center justify-center ${activeMenu === item.key ? 'active' : ''}`}
+          key={item.key}
+          onClick={() => onClickMenu(item.key)}
+        >
+          <div className="mb-1.5 w-fit">
+            <SVGIcon name={item.key} size={item.size} active={activeMenu === item.key} />
+          </div>
+          <div className="text-12 mt-1.5 w-fit font-bold text-[#757575] group-[&.active]:text-blue-400">
+            <p>{item.name}</p>
+          </div>
+        </li>,
+      );
+    });
+    return result;
+  };
+
   return (
     <nav className="h-[117px] w-full transition-all">
       <div className="fixed bottom-0 left-0 z-50 h-[70px] w-full rounded-t-[20px] bg-white shadow-[0_-4px_8px_rgba(0,0,0,0.08)]">
         <ul className="max-container flex h-full items-center justify-between py-2.5">
-          <li className="group flex grow cursor-pointer justify-center">
-            <div className="text-12 text-gray-136 group-[&.active]:text-primary font-medium">
-              <p>home</p>
-            </div>
-          </li>
-          <li className="group flex grow cursor-pointer justify-center">
-            <div className="text-12 text-gray-136 group-[&.active]:text-primary font-medium">
-              <p>커뮤니티</p>
-            </div>
-          </li>
+          {makeNavigation()}
         </ul>
       </div>
     </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,30 @@ const plugin = require('tailwindcss/plugin');
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        30: ['30px', '100%'],
+        24_32: ['24px', '32px'],
+        24: ['24px', '100%'],
+        20_28: ['20px', '28px'],
+        20: ['20px', '100%'],
+        19_28: ['19px', '28px'],
+        18_28: ['18px', '28px'],
+        18: ['18px', '100%'],
+        17_28: ['17px', '28px'],
+        16_24: ['16px', '24px'],
+        16: ['16px', '100%'],
+        15: ['15px', '100%'],
+        14_24: ['14px', '24px'],
+        14_22: ['14px', '22px'],
+        14_19: ['14px', '19px'],
+        14: ['14px', '100%'],
+        12_18: ['12px', '18px'],
+        12: ['12px', '100%'],
+        10_14: ['10px', '14px'],
+        10: ['10px', '100%'],
+      },
+    },
   },
   plugins: [
     plugin(function ({ addComponents, theme }) {
