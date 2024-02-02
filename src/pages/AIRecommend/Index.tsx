c47ed175c8d302b041622ccd75db9c7453a27359
henry <redbee99@naver.com>
2024-02-02 17:23:48 +0900

    ✨ (feat): 헤더 기능 추가 및 페이지 레이아웃 조정, SVGIcon 컴포넌트 작업

--- src/pages/AIRecommend/Index.tsx
+++ src/pages/AIRecommend/Index.tsx
@@ -1,10 +1,15 @@
+import Header from '@/components/layouts/Header';
+
 const AIRecommend = () => {
   return (
-    <div className="page">
-      <div className="max-container">
-        <p className="text-20 text-red-500">AIRecommend page 입니다.</p>
+    <>
+      <Header />
+      <div className="page mt-12">
+        <div className="max-container">
+          <p className="text-20 text-red-500">AIRecommend page 입니다.</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
